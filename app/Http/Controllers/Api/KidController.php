57ae2af6a2c73df247e17babc3a0c10810e2dea3
rebecabernal <rebecabernalmesa@gmail.com>
2024-12-13 19:02:13 +0100

    feat: Api/KidController CRUD

--- app/Http/Controllers/Api/KidController.php
+++ app/Http/Controllers/Api/KidController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Kid;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redirect;
 
 class KidController extends Controller
 {
@@ -12,23 +14,33 @@ class KidController extends Controller
      */
     public function index()
     {
-        //
+        $kids = Kid::all();
+        return response()->json($kids, 200);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+/*     public function create()
     {
         //
-    }
+    } */
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $kid = Kid::create([
+            'name' => $request->name,
+            'surname' => $request->surname,
+            'photo' => $request->photo,
+            'age' => $request->age,
+            'behaviour' => $request->behaviour,
+        ]);
+        $kid->save();
+
+        return response()->json($kid, 200);
     }
 
     /**
@@ -36,23 +48,36 @@ class KidController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $kid = Kid::find($id);
+
+        return response()->json($kid, 200);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+/*     public function edit(string $id)
     {
         //
-    }
+    } */
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
     {
-        //
+        $kid = Kid::find($id);
+        $kid ->update([
+            'name' => $request->name,
+            'surname' => $request->surname,
+            'photo' => $request->photo,
+            'age' => $request->age,
+            'behaviour' => $request->behaviour,
+        ]);
+
+        $kid -> save();
+
+        return response()->json($kid, 200);
     }
 
     /**
@@ -60,6 +85,7 @@ class KidController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $kid = Kid::find($id);
+        $kid -> delete();
     }
 }
