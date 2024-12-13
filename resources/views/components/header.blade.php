d091977adc6161462508ea2dab20154198a32620
talenaa <talenabarbosa7@gmail.com>
2024-12-13 19:20:50 +0100

    feat: added nav in header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,12 @@
 <header>
     <h1>🎁 HO HO HO!! Welcome to Santa's Toy Factory 🎄</h1>
+        <section>
+            <nav>
+                <ul>
+                    <li><a href="shop.html">Shop</a></li>
+                    <li><a href="aboutus.html">About us</a></li>
+                    <li><a href="contactus.html">Contact us</a></li>
+                </ul>
+            </nav>
+        </section>
 </header>
